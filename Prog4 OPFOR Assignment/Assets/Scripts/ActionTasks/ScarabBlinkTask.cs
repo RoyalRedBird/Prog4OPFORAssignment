a28315a065c3bf764089f59d102781c5494f8cc4
RoyalRedBird <33841152+RoyalRedBird@users.noreply.github.com>
2026-03-31 20:07:43 -0400

    Move blink functionality to behavior tree.

---
+++ Prog4 OPFOR Assignment/Assets/Scripts/ActionTasks/ScarabBlinkTask.cs
@@ -0,0 +1,44 @@
+using NodeCanvas.Framework;
+using ParadoxNotion.Design;
+using UnityEngine;
+
+
+namespace NodeCanvas.Tasks.Actions {
+
+	public class ScarabBlinkTask : ActionTask {
+
+		public BBParameter<float> BlinkTimerBBP;
+		public BBParameter<float> baseLightIntensityBBP;
+        public BBParameter<Light> ScarabLightBBP;
+
+		private float blinkTimer;
+		private float lightIntensity;
+
+		//This is called once each time the task is enabled.
+		//Call EndAction() to mark the action as finished, either in success or failure.
+		//EndAction can be called from anywhere.
+		protected override void OnExecute() {
+
+			blinkTimer = BlinkTimerBBP.value;
+			ScarabLightBBP.value.intensity = baseLightIntensityBBP.value;
+
+		}
+
+		//Called once per frame while the action is active.
+		protected override void OnUpdate() {
+			
+			blinkTimer -= Time.deltaTime;
+			ScarabLightBBP.value.intensity -= Time.deltaTime * 20;
+
+
+			if(blinkTimer < 0) { 
+			
+				EndAction(true);
+			
+			}
+
+        }
+
+	}
+
+}
